4a7304d00fb1e112070edd73a2a5dafcff26e91b
camilo <cpiescha99@gmail.com>
2025-01-14 22:13:37 -0500

    agregando base de datos

--- tmp/extract_api_nasa.py
+++ tmp/extract_api_nasa.py
@@ -2,8 +2,29 @@ import requests
 import os
 import json
 from dotenv import load_dotenv
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
 load_dotenv()
+
+MONGO_URI_CLOUD = os.getenv("MONGO_URI2")  # DATOS PARA ACCEDER A MONGO ATLAS CLOUD
+DB_NAME_CLOUD = "etlprocess"             
+COLLECTION_NAME_CLOUD= "nasa"         
+
+MONGO_URI_LOCAL = "mongodb://localhost:27017" # DATOS PARA ACCEDER A MONGO COMPASS LOCAL
+DB_NAME_LOCAL = "etlprocess"             
+COLLECTION_NAME_LOCAL = "colleccion1"         
+
+
+# Conectar a MongoDB CLOUD
+client1 = MongoClient(MONGO_URI_CLOUD)
+db1 = client1[DB_NAME_CLOUD]
+collection1 = db1[COLLECTION_NAME_CLOUD]
+
+# Conectar a MongoDB LOCAL
+client2 = MongoClient(MONGO_URI_LOCAL)
+db2 = client2[DB_NAME_LOCAL]
+collection2 = db1[COLLECTION_NAME_LOCAL]
 
 lat=6.25184
 lon=-75.56359
@@ -41,5 +62,17 @@ if response.status_code == 200:
 
     print(f"Nuevo objeto agregado al archivo {archivo_json}")
     
+
+    if response:
+        result = collection1.insert_one(response)
+        result2 = collection2.insert_one(response)# Inserta un solo documento en cloud
+        print(f"Nuevo documento insertado en cloud con ID: {result.inserted_id}")
+        print(f"Nuevo documento insertado de forma local con ID: {result2.inserted_id}")
+    else:
+        print("El archivo JSON no contiene datos válidos.")
+
+    # Cerrar la conexión
+    client1.close()
+    client2.close()  
 else:
         print(f"Error: {response.status_code}")
